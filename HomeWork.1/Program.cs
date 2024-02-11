7bdb07de4aa86a221cf2d2c8dd6b64e62fc373cb
CEYLANAhmetMuhammet <83832049+CEYLANAhmetMuhammet@users.noreply.github.com>
2024-02-11 12:31:38 +0300

    Proje dosyası ekle.

---
+++ HomeWork.1/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+//HomeWork :1
+
+using System.ComponentModel.Design;
+int number;
+Console.Write("Bir sayı giriniz:");
+number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Sayi çiftir.");
+}
+else
+{
+    Console.WriteLine("Sayi tekdir.");
+}
+Console.ReadKey();
+
+
+
+
+Console.WriteLine("Ondalıklı bir sayı giriniz.");
+
+double number2=Convert.ToDouble(Console.ReadLine());
+if (number2 % 2 == 0)
+{
+    Console.WriteLine("sayı ondalıklıdır");
+
+        
+        
+        }
+
+
+else
+    Console.WriteLine("sayı ondalıklı değildir");
+
+Console.ReadKey();
+
+
+
+
+
